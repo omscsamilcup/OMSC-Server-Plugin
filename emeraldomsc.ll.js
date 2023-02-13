3b863cc80047bfb9a6d1ee216f5b6ee1cf51589d
omscsamilcup <bianca12201220@gmail.com>
2023-02-14 00:10:50 +0800

    Update emeraldomsc.ll.js

--- emeraldomsc.ll.js
+++ emeraldomsc.ll.js
@@ -1446,8 +1446,8 @@ mc.listen('onServerStarted',()=> {
 mc.listen('onCmdBlockExecute',(isMinecart,pos) => {
     if (isMinecart == true) {
         log('有玩家使用命令方塊礦車惡意崩服，坐標為' + pos)
-    }
     return false
+    }    
 })
 
 //指南針選單
