4e3035f9ac6f958e25d807a2442f2edc663a84c2
omscsamilcup <bianca12201220@gmail.com>
2023-08-11 20:52:39 +0800

    belowname add rank

--- skyblockomsc.ll.js
+++ skyblockomsc.ll.js
@@ -25,7 +25,7 @@ setInterval(() => {
         } else if (pl.hasTag('team')) {
             rank = '§6Server Team'
         } else {
-            rank = 'Null'
+            rank = 'None'
         }
 
         var str0 = '§l§e| §r§b玩家名稱$name'.replace('$name',pl.realName)
@@ -4947,7 +4947,49 @@ setInterval(() => {
         } else if (ping >= 250) {
             ping = '§c' + ping 
         } 
+        var rank = ''
+        if (pl.hasTag('team')) {
+            if (pl.hasTag('owner')) {
+                rank = '§cOwner'
+            } else if (pl.hasTag('coop')) {
+                rank = '§bCo-Owner'
+            } else if (pl.hasTag('admin')) {
+                rank = '§gAdmin'
+            } else if (pl.hasTag('builder')) {
+                rank = '§aBuilder'
+            } else if (pl.hasTag('develop')) {
+                rank = '§bPlugins Develop'
+            } else if (pl.hasTag('helpadmin')) {
+                rank = '§9Admin Helper'
+            }
+        } else if (pl.hasTag('donate')) {
+            if (pl.hasTag('mvp')) {
+                rank = '§bMVP(D)'
+            } else if (pl.hasTag('mvpp')) {
+                rank = '§bMVP§c+(D)'
+            } else if (pl.hasTag('mvppp')) {
+                rank = '§bMVP§c++(D)'
+            } else if (pl.hasTag('vip')) {
+                rank = '§aVIP(D)'
+            } else if (pl.hasTag('vipp')) {
+                rank = '§aVIP§6+(D)'
+            }
+        } else if (!pl.hasTag('donate')) {
+            if (pl.hasTag('mvp')) {
+                rank = '§bMVP'
+            } else if (pl.hasTag('mvpp')) {
+                rank = '§bMVP§c+'
+            } else if (pl.hasTag('vippp')) {
+                rank = '§aVIP§6++'
+            } else if (pl.hasTag('vip')) {
+                rank = '§aVIP'
+            } else if (pl.hasTag('vipp')) {
+                rank = '§aVIP§6+'
+            } else {
+                rank = 'None'
+            }
+        }
         
-        pl.rename('§b>§d'+pl.realName + '§b<\n§b>§a設備:' + os + '§b<\n§b>§6延遲:' + ping + '§b<\n§b>§eLv.' + pl.getScore('level') + '§b<')
+        pl.rename('§b>§d'+pl.realName + '§b<\n§b>§a設備:' + os + '§b<\n§b>§6延遲:' + ping + '§b>§9Rank:' +rank +'§b<\n§b>§eLv.' + pl.getScore('level') + '§b<')
     })
 },1000)
