a896d16836abdd7f1ad7c6c74f0ad00bfbd74c06
omscsamilcup <bianca12201220@gmail.com>
2023-08-21 22:00:25 +0800

    /lottery調整 /pay還在修復

--- skyblockomsc.ll.js
+++ skyblockomsc.ll.js
@@ -4631,34 +4631,34 @@ mc.listen("onServerStarted",() => {
     cmd.overload(['money','bouns'])
     cmd.setCallback((_cmd,ori,_out,res) => {
         const {money,bouns} = res
-        if (ori.player.getScore('money') >= money && bouns <= ori.player.getScore('money') / 2) {
-            if (money >= 100000) {
+        if (ori.player.getScore('money') >= money && bouns <= 5) {
+            if (money >= 1000000) {
                 ori.player.reduceScore('money', money)
                 var numbers = Math.floor(Math.random() * 1001)
-                if (numbers >= 0 && numbers < 210) {
+                if (numbers >= 0 && numbers < 150) {
                     ori.player.tell('§l§e你成功抽到Bouns了，你獲得了' + money *(bouns / 2))
                     ori.player.addScore('money',money * bouns)
-                } else if (numbers >= 210 && numbers < 510) {
+                } else if (numbers >= 150 && numbers < 500) {
                     ori.player.tell('§l§b你成功抽到了Normal，你獲得了' + money)
                     ori.player.addScore('money', money)
-                } else if (numbers >= 510 && numbers < 1000) {
-                    ori.player.tell('§l§c哈哈，你運氣真的差，抽到了Null，你的錢都沒了')
+                } else if (numbers >= 500 && numbers < 1000) {
+                    ori.player.tell('§l§c哈哈，你運氣真的差，抽到了空氣，你的錢都沒了')
                 } else if (numbers == 1000) {
                     ori.player.tell('§l§d你簡直是天選之子，抽到了百分之0.1機率的鎬子和Bouns')
                     ori.player.giveItem(mc.newItem(NBT.parseSNBT('{"CanDestroy":["minecraft:emerald_block","minecraft:emerald_ore"],"Count":1b,"Damage":0s,"Name":"minecraft:netherite_pickaxe","WasPickedUp":0b,"tag":{"Damage":0,"RepairCost":0,"display":{"Name":"§l§c抽獎特別鎬子"},"ench":[{"id":18s,"lvl":30s},{"id":15s,"lvl":30s},{"id":17s,"lvl":25s},{"id":26s,"lvl":15s}]}}')))
-                    ori.player.addScore('money',money * bouns)
+                    ori.player.addScore('money',money *( bouns / 2))
                 }
             } else if (money < 100000) {
                     ori.player.reduceScore('money', money)
                 var numbers = Math.floor(Math.random() * 1001)
-                if (numbers >= 0 && numbers < 210) {
+                if (numbers >= 0 && numbers < 150) {
                     ori.player.tell('§l§e你成功抽到Bouns了，你獲得了' + money * (bouns / 2))
                     ori.player.addScore('money',money * bouns)
-                } else if (numbers >= 210 && numbers < 510) {
+                } else if (numbers >= 150 && numbers < 500) {
                     ori.player.tell('§l§b你成功抽到了Normal，你獲得了' + money)
                     ori.player.addScore('money', money)
-                } else if (numbers >= 510 && numbers < 1000) {
-                    ori.player.tell('§l§c哈哈，你運氣真的差，抽到了Null，你的錢都沒了')
+                } else if (numbers >= 500 && numbers <= 1000) {
+                    ori.player.tell('§l§c哈哈，你運氣真的差，抽到了空氣，你的錢都沒了')
                 }
             }
         }
